2f4acea26ce9c9919b8368edc159f2946a6cad8f
Onins <naustral@ncmh.gov.ph>
2025-09-30 15:14:31 +0800

    Vite config fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Aquamarine/',
+  base: '/aquamarine_frontend/',
   plugins: [
     react(),
     tailwindcss(),
